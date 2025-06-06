8bfcd8ee65f16ed6cc943935dc92cafa935fda8e
Dario Ramirez <59343230+Tirano61@users.noreply.github.com>
2025-06-06 18:28:46 -0300

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,26 @@
+
+import 'package:blocs_app/presentation/blocs/01-simple_cubit/username_cubit.dart';
 import 'package:flutter/material.dart';
 import 'package:blocs_app/config/config.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
-void main() => runApp(const MyApp());
+void main() {
+  runApp(const AppBlocProvider());
+} 
+
+class AppBlocProvider extends StatelessWidget {
+  const AppBlocProvider({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider<UsernameCubit>(create: (context) => UsernameCubit()),
+      ],
+      child: const MyApp(),
+    );
+  }
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
